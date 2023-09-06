227ca59ea4bcdf371dba50e6f90301b63294a04e
SeungjaeLim <seungjaelim@kaist.ac.kr>
2023-09-07 03:32:49 +0900

    Experiment Logging for hellaswag

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ sys.path.append("picoGPT")
 from gpt2 import gpt2, softmax
 from utils import load_encoder_hparams_and_params
 
+from datasets import load_dataset
+
+import pandas as pd
+
+logs = []
 
 def max_fn(x):
     x_max = np.where(x > 0, x, 0)
@@ -23,7 +28,6 @@ def sample(p):
 def autoregressive_sampling(x, model, N):
     n = len(x)
     T = len(x) + N
-
     with tqdm(total=N, desc="autoregressive sampling") as pbar:
         while n < T:
             x = np.append(x, sample(model(x)[-1]))
@@ -38,6 +42,7 @@ def speculative_sampling(x, draft_model, target_model, N, K):
     # we have to add an extra -1 term when indexing using n, T, or t
     n = len(x)
     T = len(x) + N
+    log = []
 
     with tqdm(total=N, desc="speculative sampling") as pbar:
         while n < T:
@@ -61,10 +66,12 @@ def speculative_sampling(x, draft_model, target_model, N, K):
                 if np.random.random() < min(1, q[i][j] / p[i][j]):  # accepted
                     x = np.append(x, j)
                     n += 1
+                    log.append(0)
                 else:  # rejected
                     x = np.append(x, sample(max_fn(q[i] - p[i])))  # resample
                     n += 1
                     all_accepted = False
+                    log.append(1)
                     break
 
             # Step 4: if all draft tokens were accepted, sample a final token
@@ -75,7 +82,7 @@ def speculative_sampling(x, draft_model, target_model, N, K):
             # just keeping my sanity
             pbar.update(n - prev_n)
             assert n == len(x), f"{n} {len(x)}"
-
+    logs.append(log)
     return x
 
 
@@ -93,17 +100,20 @@ def create_model_fn(params, hparams, temperature, eps=1e-10):
 
 def main(
     prompt: str = "Alan Turing theorized that computers would one day become",
-    n_tokens_to_generate: int = 40,
+    n_tokens_to_generate: int = 200,
     draft_model_size: str = "124M",
     target_model_size: str = "1558M",
     models_dir: str = "models",
-    K: int = 4,
+    K: int = 10,
     temperature: float = 0.0,
     seed: int = 123,
 ):
     # seed numpy rng
     np.random.seed(seed)
-
+    tot_auto = 0
+    tot_sps = 0
+    dataset = load_dataset("hellaswag")
+    
     # load encoder, hparams, and params from the released open-ai gpt-2 files
     encoder, draft_hparams, draft_params = load_encoder_hparams_and_params(
         draft_model_size, models_dir
@@ -114,46 +124,62 @@ def main(
     draft_model = create_model_fn(draft_params, draft_hparams, temperature)
     target_model = create_model_fn(target_params, target_hparams, temperature)
 
-    # encode inputs
-    input_ids = encoder.encode(prompt)
-
+    
     def run_sampling_fn(decode_fn, input_ids, **kwargs):
         start = time.perf_counter()
         output_ids = decode_fn(x=input_ids, **kwargs)
         text = encoder.decode(output_ids)
         elapsed_time = time.perf_counter() - start
         return text, elapsed_time
+    
+    for i, entry in enumerate(tqdm(dataset['train'])):
+        if i != 0 and i % 2 == 0:
+            max_length = max(map(len, logs))
+            logs_tmp = [i + [-1] * (max_length - len(i)) for i in logs]
+            df_logs = pd.DataFrame(logs_tmp)
+            with open("throughput.txt", "w") as f:
+                f.write("Autoregressive Decode\n")
+                f.write("---------------------\n")
+                f.write(f"Time = {tot_auto:.2f}s\n")
+                f.write("\n")
+                f.write("Speculative Decode\n")
+                f.write("------------------\n")
+                f.write(f"Time = {tot_sps:.2f}s\n")
+            df_logs.to_csv("log.csv", index = False)
+            
+        prompt = entry['ctx']
 
-    # autoregressive
-    autoregressive_text, autoregressive_time = run_sampling_fn(
-        autoregressive_sampling,
-        input_ids,
-        model=target_model,
-        N=n_tokens_to_generate,
-    )
-
-    # speculative
-    speculative_text, speculative_time = run_sampling_fn(
-        speculative_sampling,
-        input_ids,
-        target_model=target_model,
-        draft_model=draft_model,
-        N=n_tokens_to_generate,
-        K=K,
-    )
-
-    # print results
-    print()
-    print("Autoregressive Decode")
-    print("---------------------")
-    print(f"Time = {autoregressive_time:.2f}s")
-    print(f"Text = {autoregressive_text}")
-    print()
-    print("Speculative Decode")
-    print("------------------")
-    print(f"Time = {speculative_time:.2f}s")
-    print(f"Text = {speculative_text}")
-
+        input_ids = encoder.encode(prompt)
+        
+        autoregressive_text, autoregressive_time= run_sampling_fn(
+            autoregressive_sampling,
+            input_ids,
+            model=target_model,
+            N=n_tokens_to_generate,
+        )
+        
+        speculative_text, speculative_time= run_sampling_fn(
+            speculative_sampling,
+            input_ids,
+            target_model=target_model,
+            draft_model=draft_model,
+            N=n_tokens_to_generate,
+            K=K,
+        )
+        tot_auto += autoregressive_time
+        tot_sps += speculative_time
+        # print results
+        #print()
+        #print("Autoregressive Decode")
+        #print("---------------------")
+        #print(f"Time = {autoregressive_time:.2f}s")
+        #print(f"Text = {autoregressive_text}")
+        #print()
+        #print("Speculative Decode")
+        #print("------------------")
+        #print(f"Time = {speculative_time:.2f}s")
+        #print(f"Text = {speculative_text}")
+        
 
 if __name__ == "__main__":
     import fire
